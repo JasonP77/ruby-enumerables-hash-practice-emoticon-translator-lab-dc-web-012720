0761205519c60598892c6168b69fe86632548d27
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 21:09:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ def get_japanese_emoticon(file_path, western_emoticon)
   library = load_library(file_path)
   if lirbary["get_emoticon"].include?(western_emoticon)
     library["get_emoticon"][western_emoticon]
+  end
 end
 
 def get_english_meaning
