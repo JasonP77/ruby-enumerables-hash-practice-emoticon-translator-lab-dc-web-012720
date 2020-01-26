62e5da2ceee069eb1ca3bc315cf00614f01448c5
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 20:44:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,12 @@
 require "yaml"
 def load_library(file)
   collection = YAML.load_file(file)
-  
+  final = {"mean" => {}, "emoticons" => {}}
+  library.each do |meaning, emoticons|
+    result["mean"][emoticons[1]] = meaning
+    result["emoticons"][emoticons[0]] = emoticons[1]
+  end
+  result
 end
 
 def get_japanese_emoticon
