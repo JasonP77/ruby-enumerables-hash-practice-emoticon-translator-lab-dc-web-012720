3cb14197634512b2386b012b3234a4c3cbde3467
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 21:15:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,10 +10,10 @@ def load_library(file_path)
   result
 end
 
-def get_japanese_emoticon(file_path , english_emoticon)
+def get_japanese_emoticon(file_path , western_emoticon)
   library = load_library(file_path)
-  if library["get_emoticon"].include?(english_emoticon) 
-    library["get_emoticon"][english_emoticon]
+  if library["get_emoticon"].include?(western_emoticon) 
+    library["get_emoticon"][western_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
